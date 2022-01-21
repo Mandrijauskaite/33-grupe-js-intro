1f3ee57ed731a33a8889b620bb366562f99c4fd1
Mandrijauskaite <97032997+Mandrijauskaite@users.noreply.github.com>
2022-01-21 14:04:06 +0200

    Array

--- js/01-variables.js
+++ js/01-variables.js
@@ -58,3 +58,52 @@ const sakinys = pirmasZodis + " " + antrasZodis + " ";
 console.log(1, pirmasZodis);
 console.log(2, antrasZodis);
 console.log(3, sakinys);
+
+console.log ("--------------------");
+
+const kabutes1 = "It`s my name";
+console.log(kabutes1);
+
+const kabutes2 = `Tai vadinasi "Cepelinas" ar kazkas panasiai`;
+console.log(kabutes2);
+
+console.log ("--------------------");
+
+const kabutes12 = 'Cia yra vienguba (\'), o cia yra dviguba (") kabutes.';
+const kabutes21 = "Cia yra vienguba ('), o cia yra dviguba (\") kabutes.";
+console.log(kabutes12);
+console.log(kabutes21);
+
+console.log ("--------------------");
+
+const kabutes120 = 'Cia yra vienguba (\'), o cia yra dviguba (\") kabutes.';
+const kabutes210 = "Cia yra vienguba (\'), o cia yra dviguba (\") kabutes.";
+console.log(kabutes120);
+console.log(kabutes210);
+
+console.log ("--------------------");
+
+// Pagrindinis aktorius yra Chuck Norris filme "Volkeris. Teksaso reindzeris"
+const aktoriausVardas = "Chuck";
+const aktoriausPavarde = "Norris";
+const filmoPavadinimas = "Volkeris. Teksaso reindzeris"
+
+const filmas1 = `Pagrindinis aktorius yra ` + aktoriausVardas + ` ` + aktoriausPavarde + ` filme "` + filmoPavadinimas + `".`;
+console.log(1, filmas1);
+
+const filmas2 = "Pagrindinis aktorius yra " + aktoriausVardas + " " + aktoriausPavarde + " filme \"" + filmoPavadinimas + "\".";
+console.log(2, filmas2);
+
+const filmas3 = `Pagrindinis aktorius yra ${aktoriausVardas} ${aktoriausPavarde} filme "${filmoPavadinimas}"`;
+console.log(3, filmas3);
+
+console.log ("--------------------");
+
+// Viengubos ('), dvigubos ("), backtick'as (`).
+const kabuciuCombo1 = 'Viengubos (\'), dvigubos ("), backtick\'as (`).';
+const kabuciuCombo2 = "Viengubos ('), dvigubos (\"), backtick'as (`).";
+const kabuciuCombo3 = `Viengubos ('), dvigubos ("), backtick'as (\`).`;
+
+console.log(1, kabuciuCombo1);
+console.log(2, kabuciuCombo2);
+console.log(3, kabuciuCombo3);
---
+++ js/02-array.js
@@ -0,0 +1,29 @@
+// Array - masyvas, sarasas, listas, matrica
+
+
+
+const pazymiai = [10, 2, 8, 6, 4];
+console.log(pazymiai);
+
+const asmuo1 = 'Petras';
+const asmuo2 = 'Ona';
+const asmuo3 = 'Jonas';
+const asmuo4 = 'Maryte';
+
+const asmenys = [asmuo1, asmuo2, asmuo3, asmuo4];
+console.log(asmenys);
+
+console.log('----------------');
+
+const maistoSarasas = ['pomidoras', 'agurkas', 'bulve', 'svogunas'];
+console.log(maistoSarasas);
+
+console.log('Reikia nupirkti:', maistoSarasas[0]);
+console.log('Reikia nupirkti:', maistoSarasas[1]);
+console.log('Reikia nupirkti:', maistoSarasas[2]);
+console.log('Reikia nupirkti:', maistoSarasas[3]);
+
+console.log('-----------');
+
+// Pazymiai: 10, 2, 8, 4.
+// Pazymiu suma:
