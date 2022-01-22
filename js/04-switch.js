b10685864d730ca006bd727ad0e503f1ef76c7d5
Mandrijauskaite <97032997+Mandrijauskaite@users.noreply.github.com>
2022-01-22 22:30:49 +0200

    switch

---
+++ js/04-switch.js
@@ -0,0 +1,44 @@
+const diena = 2;
+
+switch (diena) {
+    case 1:
+        console.log('pirmadienis');
+        break;
+
+        case 2:
+            console.log('antradienis');
+            break;
+
+        case 3:
+            console.log('treciadienis');
+            break;
+
+        case 4:
+            console.log('ketvirtadienois');
+            break;
+
+        case 5:
+            console.log('penktadienis');
+            break;
+}
+
+console.log('----------');
+
+const dd = 8;
+
+switch (dd) {
+    case 1:
+        case 2:
+            case 3:
+                case 4:
+                    case 5:
+                        console.log('darbo diena');
+                        break;
+                        case 6:
+                            case 7:
+                            console.log('savaitgalis');
+                            break;
+                            default:
+                                console.log('tokia diena neegzistuoja');
+}
+
