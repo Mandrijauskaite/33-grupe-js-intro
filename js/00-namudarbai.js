2384a1f11925c91bf0410d9c3018ca821195d0c5
Mandrijauskaite <97032997+Mandrijauskaite@users.noreply.github.com>
2022-01-23 21:36:56 +0200

    Namu darbai

---
+++ js/00-namudarbai.js
@@ -0,0 +1,83 @@
+console.log('------------');
+// Kintamuju iniciavimas
+// 1. Sukurti 3 kintamuosius su skaiciaus tipo reiksmemis. 
+// a) po kiekvieno ju inicijavimo, isvesti console
+
+const pirmas = 4;
+console.log(pirmas);
+
+const antras = 6;
+console.log(antras);
+
+const trecias = 7;
+console.log(trecias);
+
+console.log('------------');
+// 2. Sukurti 3 kintamuosius su teksto tipo reiksmemis. 
+// a) po kiekvieno ju inicijavimo, isvesti console
+
+const miestas = 'Šilutė';
+console.log(miestas);
+
+const rajonas = 'Šilutės raj.';
+console.log(rajonas);
+
+const apskritis = 'Klaipėdos apskr.'
+console.log(apskritis);
+
+console.log('------------');
+// 3. Sukurti 3 saraso tipo kintamuosius su penkiomis skaiciu tipo reiksmemis. 
+// a) po kiekvieno ju inicijavimo, isvesti console
+
+let batuKiekis = 1;
+console.log('Batų porų parduotuvėje yra:', batuKiekis);
+
+const pirmiBatai = 9;
+console.log('I-oje siuntoje yra batų porų:', pirmiBatai);
+
+batuKiekis = batuKiekis + pirmiBatai;
+console.log('Batų porų parduotuvėje yra:', batuKiekis);
+
+const antriBatai = 8;
+console.log('II-oje siuntoje yra batų porų:', antriBatai);
+
+batuKiekis = batuKiekis + antriBatai;
+console.log('Batų porų parduotuvėje yra:', batuKiekis);
+
+const tretiBatai = 11;
+console.log('III-oje siuntoje yra batų porų:', tretiBatai);
+
+batuKiekis = batuKiekis + tretiBatai;
+console.log('Batų porų parduotuvėje yra:', batuKiekis);
+
+const ketvirtiBatai = 20;
+console.log('IV-oje siuntoje yra batų porų:', ketvirtiBatai);
+
+batuKiekis = batuKiekis + ketvirtiBatai;
+console.log('Batų porų parduotuvėje yra:', batuKiekis);
+
+const penktiBatai = 20;
+console.log('V-oje siuntoje yra batų porų:', penktiBatai);
+
+batuKiekis = batuKiekis + penktiBatai;
+console.log('Batų porų parduotuvėje yra:', batuKiekis);
+
+onsole.log('------------');
+// 4. Sukuri 3 kintamuosius su teksto tipo reiksmemis. 
+// a) po kiekvieno ju inicijavimo, isvesti console
+// Kaziuko mugė įvyks 2022 metų kovo 3 dieną. Mugės pradžia 10 val.
+
+const metuMenuo = "kovo";
+const menesioDiena = 3;
+const parosValanda = 10;
+
+const renginys = `Kaziuko mugė įvyks 2022 metų ${metuMenuo} ${menesioDiena} dieną. Mugės pradžia ${parosValanda} valandą.`;
+console.log(renginys);
+
+const renginys1 = "Kaziuko mugė įvyks 2022 metų" + " " + metuMenuo + " " + menesioDiena + " " + "dieną.  Mugės pradžia" + " " + parosValanda + " " + "valandą.";
+console.log(renginys1);
+
+onsole.log('TESTAVIMAS');
+
+const kabutes1 = "It`s my name";
+console.log(kabutes1);
