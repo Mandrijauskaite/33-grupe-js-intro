a59e13f5d860089b9de348e47ebc594d79598f2c
Mandrijauskaite <97032997+Mandrijauskaite@users.noreply.github.com>
2022-01-24 21:41:30 +0200

    For pratybos

---
+++ js/09-count.js
@@ -0,0 +1,11 @@
+function count (n) {
+    const numberAsString = '' + n;
+    return 0;
+}
+
+console.log(count(5), '-->', 1);
+console.log(count(781), '-->', 3);
+// console.log(count(5), '-->', 1);
+// console.log(count(5), '-->', 1);
+// console.log(count(5), '-->', 1);
+// console.log(count(5), '-->', 1);
