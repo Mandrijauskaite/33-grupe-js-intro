b152ec10854cc2ee5698e884c8507707d314fe9c
Mandrijauskaite <97032997+Mandrijauskaite@users.noreply.github.com>
2022-01-27 22:07:30 +0200

    Loops

--- js/14-loops.js
+++ js/14-loops.js
@@ -13,7 +13,7 @@ for (let i=0; i<list.length; i++) {
     console.log(item);
 }
 
-//WHILE (maziau efektyvus nei FOR)
+// WHILE (maziau efektyvus nei FOR)
 console.log('WHILE: --------------');
 let iw = 0;
 while (iw < list.length) {
@@ -31,7 +31,7 @@ while (rn < 0.9 && ir <imax) {
     ir++;
 }
 
-//D-WHILE (panasu i WHILE ideja pirma duoda i snuki po to klausia)
+//DO-WHILE (panasu i WHILE ideja pirma duoda i snuki po to klausia)
 console.log('DO-WHILE: --------------');
 let idw = 0;
 do {
@@ -39,3 +39,50 @@ do {
         console.log(item);
         idw++
 } while (idw < list.length);
+
+//MAP 
+console.log('MAP: --------------');
+const mapMarks = [10, 2, 8, 4, 6];
+const doubleMapMarks = mapMarks.map(n => n * 2);
+
+console.log(mapMarks);
+console.log(doubleMapMarks);
+
+
+const listFirstLetters = list.map(item => item[0]);
+const listLengths = list.map(item => item.length);
+
+console.log(list);
+console.log(listFirstLetters);
+console.log(listLengths);
+
+const nums = [21487, 45852, 84562, 859624];
+const numsFirstNum = nums.map(n => +('' + n)[0]);
+console.log(numsFirstNum);
+
+// FILTER
+console.log('FILTER: --------------');
+const filterMarks = [10, 2, 8, 4, 6];
+const goodMarks = filterMarks.filter(n => n >= 7);
+
+console.log(filterMarks);
+console.log(goodMarks);
+
+
+// MAP + FILTER
+console.log('MAP + FILTER: --------------');
+const names = ['Jonas', 'Ona', 'Rasa', 'Petras', 'Maryte'];
+// pasilikti tik vardu pirmas raides, kur vardu ilgis maziau 6 simb.
+const modifiedNames = names
+.filter(name => name.length < 6)
+.map(name => name[0]);
+console.log(modifiedNames);
+
+const modifiedNames2 = names
+.map(name => name[name.length - 1])
+.filter(name => name.length < 6);
+console.log(modifiedNames2);
+
+// SORT
+// REDUCE
+// FOR-EACH
